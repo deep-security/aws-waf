757d0f154adb76e4c7243f3e4738a1378a08db88
Mark Nunnikhoven <me@markn.ca>
2016-03-06 06:48:21 +0900

    Migrated code from original implementation to new class structure

--- README.md
+++ README.md
@@ -59,3 +59,58 @@ And during execution you may see lines similar to;
 ```
 
 These are expected warnings. Can you tell that we (and the python core teams) are trying to tell you something? If you're interesting in using a valid SSL certificate, you can get one for free from [Let's Encrypt](https://letsencrypt.org), [AWS themselves](https://aws.amazon.com/certificate-manager/) (if your DSM is behind an ELB), or explore commercial options (like the [one from Trend Micro](http://www.trendmicro.com/us/enterprise/cloud-solutions/deep-security/ssl-certificates/)).
+
+## AWS WAF Costs
+
+https://aws.amazon.com/waf/pricing/
+
+AWS WAF charges based on the number of web access control lists (web ACLs) that you create, the number of rules that you add per web ACL, and the number of web requests that you receive. There are no upfront commitments. AWS WAF charges are in addition to Amazon CloudFront Pricing.
+Web Access Control List and Rule Charges
+
+AWS WAF charges based on the number of web ACLs that you create and the number of rules that you add per web ACL. Web ACLs and their added rules are metered regardless of whether they are associated with a CloudFront distribution. There is no additional charge for reusing web ACLs across mulitple CloudFront distributions; however, you are charged for each rule added to each web ACL.
+Web Access Control List Charges
+
+$5 per web ACL per month
+
+Rule Charges
+
+$1 per rule per web ACL per month
+
+Request Charges
+
+AWS WAF also charges for the amount of web request AWS WAF handles. This is based upon the number of web requests the application receives.
+
+Request Charge
+
+$0.60 per million web requests
+Manage Your AWS Resources
+
+Sign in to the Console
+Pricing Example
+Let’s assume you start using AWS WAF at the first of the month to protect eight CloudFront web distributions. For this example, we’ll create two web ACLs, one web ACL with four rules associated to six CloudFront web distributions, and another web ACL with six rules associated to two remaining CloudFront web distributions. The CloudFront web distributions are expected to receive 10 million requests per month in total.
+
+Web ACL
+
+Web ACL charge = # of web ACLs per month * $5
+
+Total web ACL charge = 2 * $5 = $10
+
+Rule
+
+The price for a rule is $1 per month (pro-rated by the hour)
+
+Rule charge = # of rules associated per month * $1
+
+Total rule charge = (4 + 6) * $1 = $10
+
+Request
+
+The price for request is $0.60 per million
+
+Request charge = # of requests per month (in millions) * $0.60
+
+Total request charge = 10 million requests * $0.60 = $6.00
+
+Total Monthly Bill
+
+Total web ACL charge + total rule charge + total request charge = $10 + $10 + $6 = $26
--- lib/core.py
+++ lib/core.py
@@ -53,4 +53,90 @@ class ScriptContext():
     """
     Print the command line syntax available to the user
     """
-    self.parser.print_help()   
+    self.parser.print_help()
+
+  def _get_aws_credentials(self):
+    """
+    Get a set of AWS credentials from a pre-configured AWS CLI installation
+    """
+    credentials = None
+
+    # check locally for an AWS CLI installation
+    aws_credentials_path = [ '{}/.aws/credentials'.format(os.environ['HOME']), "{}\.aws\credentials".format(os.environ['HOME']) ]
+    for path in aws_credentials_path:
+      if os.path.exists(path) and not credentials:
+        self._log("Reading AWS credentials from {}".format(path))
+        with open(path) as fh:
+          for line in fh:
+            if line.startswith('aws_access_key_id'):
+              credentials = { 'aws_access_key_id': line.split('=')[-1].strip() }
+            elif line.startswith('aws_secret_access_key'):
+              credentials['aws_secret_access_key'] = line.split('=')[-1].strip()
+
+    return credentials
+
+  def _connect_to_deep_security(self):
+    dsm = None
+    if self.args.ignore_ssl_validation:
+      self._log("""***********************************************************************
+* IGNORING SSL CERTIFICATE VALIDATION
+* ===================================
+* You have requested to ignore SSL certificate validation. This is a less secure method 
+* of connecting to a Deep Security Manager (DSM). Please ensure that you have other 
+* mitigations and security controls in place (like restricting IP space that can access 
+* the DSM, implementing least privilege for the Deep Security user/role accessing the 
+* API, etc).
+*
+* During script execution, you'll see a number of "InsecureRequestWarning" messages. 
+* These are to be expected when operating without validation. 
+***********************************************************************""", priority=True)
+    try:
+      dsm = deepsecurity.manager.Manager(dsm_hostname=self.args.dsm, dsm_port=self.args.dsm_port, username=self.args.username, password=self.args.password, tenant=self.args.tenant, ignore_ssl_validation=self.args.ignore_ssl_validation) 
+      self._log("Connected to the Deep Security Manager at {}".format(self.args.dsm))
+    except Exception, err: pass # @TODO handle this exception gracefully
+
+    if not dsm.session_id_rest and not dsm.session_id_soap:
+      self._log("Unable to connect to the Deep Security Manager. Please check your settings")
+      if not self.args.ignore_ssl_validation:
+        self._log("You did not ask to ignore SSL certification validation. This is a common error when connect to a Deep Security Manager that was installed via software or the AWS Marketplace. Please set the flag (--ignore-ssl-validation), check your other settings, and try again")
+
+    return dsm 
+
+  def _connect_to_aws_waf(self):
+    waf = None
+    try:
+      aws = boto3.session.Session(aws_access_key_id=self.aws_credentials['aws_access_key_id'], aws_secret_access_key=self.aws_credentials['aws_secret_access_key'])
+      waf = aws.client('waf') 
+      self._log("Connected to AWS WAF")
+    except Exception, err: 
+      # @TODO handle this exception gracefully
+      try:
+        waf = boto3.client('waf')
+        self._log("Connected to AWS WAF")
+      except Exception, err: pass # @TODO handle this exception gracefully  
+
+    return waf
+
+  def get_available_aws_sets(self):
+    """
+    Get a list of the available IP Sets in AWS WAF
+    """
+    ip_sets = []
+    if self.waf:
+      response = self.waf.list_ip_sets(Limit=100)
+      if response and response.has_key('IPSets'):
+        for ip_set in response['IPSets']:
+          ip_sets.append(ip_set)
+
+    self.ip_sets = ip_sets
+
+    return ip_sets
+
+  def _get_aws_waf_change_token(self):
+    response = self.waf.get_change_token()
+    change_token = None
+    if response and response.has_key('ChangeToken'):
+      change_token = response['ChangeToken']
+      self._log("New AWS WAF change token [{}]".format(change_token))
+
+    return change_token
